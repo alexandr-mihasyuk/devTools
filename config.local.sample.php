978e792c278e19f40b9894ecc8b41572b02053c6
Alexandr Mihasyuk <alexandr.mihasyuk@gmail.com>
2015-06-08 13:45:51 +0300

    Update config.local.sample.php
    Added const BRANCH_FORMAT_DEPARTMENT = 'payment';

--- config.local.sample.php
+++ config.local.sample.php
@@ -5,5 +5,6 @@ class Config
     const REDMINE_URL = '{https://your.redmine.com}';
     const REDMINE_API_KEY = 'API key from your remine account';
     const BRANCH_FORMAT_SURNAME = 'your surname for using in git branches';
+    const BRANCH_FORMAT_DEPARTMENT = 'payment';
 }
 
