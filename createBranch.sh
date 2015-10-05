f48a3bb546283660e22cd6368f562ea43e0aadae
Alexandr Mihasyuk <alexandr.mihasyuk@gmail.com>
2015-10-05 14:54:24 +0300

    detect repo name in shell script

--- createBranch.sh
+++ createBranch.sh
@@ -2,12 +2,14 @@
 runScript="$HOME/redmineApiTools/createBranch.php"
 phpBin=`which php`
 branchCreator="$phpBin $runScript"
+gitRepoNameCmd=`git rev-parse --show-toplevel`
+repoName=`basename $gitRepoNameCmd`
+echo "repo: $repoName"
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "Creating branch name..."
 branch=`$branchCreator $@`
 echo "Branch name is $branch"
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-
 git stash
 git remote update
 git checkout -b $branch origin/live --track
